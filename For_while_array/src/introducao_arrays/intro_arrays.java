9a948f98fce02b1d8be7bc144a8c6b826fab87dc
Charles Patriarca <charlespatriarcati@gmail.com>
2024-06-27 16:36:14 -0300

    Atualização

---
+++ For_while_array/src/introducao_arrays/intro_arrays.java
@@ -0,0 +1,18 @@
+package introducao_arrays;
+
+public class intro_arrays {
+
+	public static void main(String[] args) {
+		/*int a[] = new int [4];
+		a[3] = 50;
+		System.out.println(a[3]);*/
+		
+		int [] intArray = {2,5,46,12,34};
+		
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray [i]);
+			
+		}
+	}
+
+}
