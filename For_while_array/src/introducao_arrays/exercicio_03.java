6457e402c1992ce46ea573602dd44ef9b3b5c77a
Charles Patriarca <charlespatriarcati@gmail.com>
2024-06-27 16:45:36 -0300

    Atualização

---
+++ For_while_array/src/introducao_arrays/exercicio_03.java
@@ -0,0 +1,12 @@
+package introducao_arrays;
+
+public class exercicio_03 {
+
+	public static void main(String[] args) {
+		double [] a = {5,7,8,9,6};
+		
+		for (double i : a)
+		System.out.printf(i + " ");
+	}
+
+}
