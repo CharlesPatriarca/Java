59d88105035067c8377012d8debb4a21e8991c59
Charles Patriarca <charlespatriarcati@gmail.com>
2024-06-27 15:23:06 -0300

    Atualização

--- introducao_java/src/introducao_java/intro_02.java
+++ introducao_java/src/introducao_java/intro_02.java
@@ -3,8 +3,12 @@ package introducao_java;
 public class intro_02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a = 10;
+		int b = 10;
+		System.out.println(a++ + ++a); // 10 + 12 = 22
+		System.out.println(b++ + b++); // 10 + 11 = 21
+		
 	}
 
 }
