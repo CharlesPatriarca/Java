2c2ac20aee2b4414ac33c0a49b84b909e5f58bf9
Charles Patriarca <charlespatriarcati@gmail.com>
2024-06-27 15:55:43 -0300

    Atualização

---
+++ Scanner_If_else/src/Scanner_If_else/exercicio_01.java
@@ -0,0 +1,24 @@
+package Scanner_If_else;
+
+import java.util.Scanner;
+
+public class exercicio_01 {
+
+	public static void main(String[] args) {
+		Scanner num1 = new Scanner (System.in);
+		System.out.println("Digite um número: ");
+		int resp = num1.nextInt();
+				
+		if (resp < 0) {
+			System.out.println("O número é negativo");
+		
+		}else if (resp > 0) {
+			System.out.println("O número é positivo");
+					
+		} else {
+			System.out.println("Número nem negativo nem positivo");
+		}
+		num1.close();
+	}
+
+}
