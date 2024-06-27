de65f707f0735a51ec5a12de96303b502a66980f
Charles Patriarca <charlespatriarcati@gmail.com>
2024-06-27 15:16:23 -0300

    Atualização

--- introducao_java/src/introducao_java/intro_01.java
+++ introducao_java/src/introducao_java/intro_01.java
@@ -2,4 +2,11 @@ package introducao_java;
 
 public class intro_01 {
 
+		public static void main(String[] args) {
+			int x = 10;
+			System.out.println (x++); // 10 (11)
+			System.out.println (++x); // 12
+			System.out.println (x--); // 12 (11)
+			System.out.println (--x); // 10
+		}
 }
