02e50c904408d52de01cca645dde2b042418c081
Charles Patriarca <charlespatriarcati@gmail.com>
2024-06-27 16:33:15 -0300

    Atualização

---
+++ For_while_array/src/introducao_for/exercicio_02.java
@@ -0,0 +1,13 @@
+package introducao_for;
+
+public class exercicio_02 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+
+		}
+	}
+
+}
