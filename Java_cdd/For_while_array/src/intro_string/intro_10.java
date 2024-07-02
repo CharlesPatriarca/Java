d235181629e8f29cf73bc00d0ae1910d2c533d6f
Charles Patriarca <charlespatriarcati@gmail.com>
2024-07-02 10:23:27 -0300

    Novo código

---
+++ Java_cdd/For_while_array/src/intro_string/intro_10.java
@@ -0,0 +1,21 @@
+package intro_string;
+
+public class intro_10 {
+
+	public static void main(String[] args) {
+		//MÉTODO QUE MOSTRA A POSIÇÃO DA LETRA NO INÍCIO
+		
+		String str = "Hello World World2";
+		int pos = str.indexOf ("l");
+		
+		System.out.println(pos);
+		
+		System.out.println();
+		
+		//MÉTODO QUE MOSTRA A POSIÇÃO DA LETRA NO FINAL
+		
+		int pos1 = str.lastIndexOf("l");
+		System.out.println(pos1);
+	}
+
+}
