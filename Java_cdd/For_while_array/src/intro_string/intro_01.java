ade60eb5488b0ce5f44408b3ab98138ea2879214
Charles Patriarca <charlespatriarcati@gmail.com>
2024-07-02 08:40:56 -0300

    Adição de Banco

---
+++ Java_cdd/For_while_array/src/intro_string/intro_01.java
@@ -0,0 +1,16 @@
+package intro_string;
+
+public class intro_01 {
+
+	public static void main(String[] args) {
+		
+		char caracteres [] = {'a', 'b', 'c'};
+		String texto = new String (caracteres);
+		System.out.println(texto);
+		
+		System.out.println();
+		
+	
+	}
+
+}
---
+++ Java_cdd/For_while_array/src/intro_string/intro_02.java
@@ -0,0 +1,10 @@
+package intro_string;
+
+public class intro_02 {
+	public static void main(String[] args) {
+		
+		String str = "Hello";
+		String resultados = str.replace("l", "w");
+		System.out.println(resultados);
+	}
+}
