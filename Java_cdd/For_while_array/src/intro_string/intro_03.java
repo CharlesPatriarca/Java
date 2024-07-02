b2c7a1268a44410badc68a3220539c608aec6951
Charles Patriarca <charlespatriarcati@gmail.com>
2024-07-02 08:50:49 -0300

    Novo código

---
+++ Java_cdd/For_while_array/src/intro_string/intro_03.java
@@ -0,0 +1,23 @@
+package intro_string;
+
+public class intro_03 {
+
+	public static void main(String[] args) {
+		
+		String str = "Oi"; String texto = str + " Mundo";
+		System.out.println(texto);
+		
+		System.out.println();
+		
+		int tres = 3;
+		String resultadoFinal = 3 + " palavra " + 3;
+		System.out.println(resultadoFinal);
+		
+		System.out.println();
+		
+		String str1 = "Hello World";
+		String resultado = str1.substring(6);
+		System.out.println(resultado);
+	}
+
+}
